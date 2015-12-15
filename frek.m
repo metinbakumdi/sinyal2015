c72639d543e89dfe5a019ba9b429be8b10d90ba6
metinbakumdi <metinbakumdi@gmail.com>
2015-12-15 11:59:16 +0200

    frek.m dosyasına kod eklendi.

--- frek.m
+++ frek.m
@@ -1,34 +1,21 @@
-function f = frek( nota , oktav )
+function ff=frek(nota,oktav) %frek fonksiyonunu tanýmladým. 
 
 
-switch nota
-    case 'C'
-        notaHz = 16.35;
-    case 'C#'
-        notaHz = 17.32;
-    case 'D'
-        notaHz = 18.35;
-    case 'Eb'
-        notaHz = 19.45;
-    case 'E'
-        notaHz = 20.60;
-    case 'F'
-        notaHz = 21.83;
-    case 'F#'
-        notaHz = 23.12;
-    case 'G'
-        notaHz = 24.50;
-    case 'G#'
-        notaHz = 25.96;
-    case 'A'
-        notaHz = 27.50;
-    case 'Bb'
-        notaHz = 29.14;
-    case 'B'
-        notaHz = 30.87;
-end
-
-f = notaHz * 2^oktav;
-
-end
-
+notalar=['A' 'a' 'B' 'b' 'C' 'D' 'd' 'E' 'e' 'F' 'f' 'G'] %notalarý tanýmladým. 
+referans=16.35; %Do notasýnýn 0. oktavdaki deðeri referans olarak belirlendi.
+p=length(notalar); %p deðiþkeni notalar matrisinin uzunluðuna eþit
+ 
+for okt=0:8  %oktav 0dan 8e kadar
+if okt==oktav %girdiðimiz oktav 0 ile 8 arasýndaysa
+for j=1:p %j deðeri 1den matrisin boyutuna kadar
+if nota==char(notalar(j)) %girilen nota notalar matrisindeki bir elemana eþitse 
+ 	 ff==2^okt*(2^((j-1)/12)*referans; %frekansý hesapla
+ 	end %if notanýn bitiþi 
+ 	end %for j'nin bitiþi 
+ 	end %if okt'un bitiþi 
+ 	end %for okt'un bitiþi
+ 	end %function'ýn bitiþi 
+ 
+ 
+ %Frek fonksiyonunun çaðýrmak için
+  f=frek('D',5) 
